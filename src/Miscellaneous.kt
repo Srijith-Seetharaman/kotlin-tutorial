0b5bea909240364493287175163ddb9c63123033
Srijith-Seetharaman <78795366+Srijith-Seetharaman@users.noreply.github.com>
2025-01-31 11:46:04 +0000

    first commit

---
+++ src/Miscellaneous.kt
@@ -0,0 +1,14 @@
+fun main() {
+    println("Hello World!")
+    println("Hi. My name is, huh? My name is, who? My name is, chka-chka, Slim Shady")
+    println("I am trying to learn Kotlin.")
+    print("All this goes ")
+    print("in the same line.\n")
+    println(3+3)
+    // Adding this comment to test out Kotlin comments
+    /*
+    This is a multi-line comment
+    in Kotlin, just like it is in Java
+     */
+    println("Tried out a comment in Kotlin, and it worked out very well.")
+}
---
+++ src/Variables.kt
@@ -0,0 +1,21 @@
+val name: String = "Srijith"
+/* Invalid code because if variables are to be declared in one places and its value
+   initialized somewhere else, the type of the variable should be bound at the point
+   of declaration
+var myName
+val myName1
+/myName1 = name
+*/
+
+fun main(){
+    val myName: String
+    myName = "Srijith1"
+    println(myName)
+//    name = "helpMe"
+    println(name)
+
+    var myAge : Int = 28
+
+    var fullString: String = myName + " " + myAge
+    println(fullString)
+}
