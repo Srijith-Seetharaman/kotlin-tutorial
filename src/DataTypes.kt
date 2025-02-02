75e7b9dd2e51d7c86066d6f5a51ad566d753db13
Srijith-Seetharaman <78795366+Srijith-Seetharaman@users.noreply.github.com>
2025-02-02 01:16:58 +0000

    Data types

---
+++ src/DataTypes.kt
@@ -0,0 +1,13 @@
+fun main(){
+    val byteVal: Byte = 120
+    println(byteVal)
+
+    val shortVal: Short = 32766
+    println(shortVal)
+
+    val intVal = 345435324
+    println(intVal)
+
+    val longVal: Long = Long.MAX_VALUE
+    println(longVal)
+}
